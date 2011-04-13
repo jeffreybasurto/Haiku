508ea24eca57d85985d13fddcb30c9bdc07b6b42
Jeffrey Basurto <jeffreybasurto@gmail.com>
2011-04-13 18:15:07 -0500

    changed server and added utilities file

--- server.rb
+++ server.rb
@@ -2,6 +2,8 @@ require 'bundler/setup'
 Bundler.require
 set :port, 3000
 
+require ''
+
 $welcome = <<-HERE
 <span style="font-size:20px;">Welcome to <img src="haikulogo.png" alt="haikumud">. <span>
 HERE
@@ -9,9 +11,12 @@ HERE
 Thread.new do
   puts "Starting websocket server."
   EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
-    ws.onopen { ws.send JSON.generate({"chat"=>$welcome})}
+    ws.onopen { ws.send JSON.generate({"scrollback"=>$welcome})}
     # When we receive a message just echo it back for now.
-    ws.onmessage { |msg| ws.send msg }
+    ws.onmessage { |msg| 
+      msg = JSON.parse(msg)
+      ws.send JSON.generate({"scrollback"=>msg}) 
+    }
     ws.onclose { puts "Connection closed." }
   end
 end
@@ -20,3 +25,5 @@ end
 get '/' do 
   erb :client, :locals=>{:host=>request.env["SERVER_NAME"]}
 end
+
+
