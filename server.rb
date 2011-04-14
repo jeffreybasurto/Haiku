6bfd3bb47b912f6df6bb70732865567f4d4f2548
Jeffrey Basurto <jeffreybasurto@gmail.com>
2011-04-14 15:10:53 -0500

    changed ui

--- server.rb
+++ server.rb
@@ -8,6 +8,11 @@ Thread.abort_on_exception = true
 load 'utilities.rb'
 
 $welcome = <<-HERE
+<script>
+  $(function() {
+$( "button, input:submit, a" ).button();
+});
+</script>
 <div style="font-size:20px;">Welcome to <img src="haikulogo.png" alt="haikumud">. </div>
 <div style="font-size:16px;color:grey;">HaikuMud (C) 2011 Jeffrey "Retnur/Runter" Basurto.</div>
 <br>
@@ -15,10 +20,11 @@ $welcome = <<-HERE
   <form name="login" onSubmit="return false;">
     <input type="text" name="user_name">  User Name <br>
     <input type="password" name="user_pass">  Password <br>
-    <input type="submit" name="login_submit" value="Log In">
+    
+    <input type="submit" name="login_submit" value="Log In" style="font-size:15;">
   </form>
 </div>
-<div>
+<div style="font-size:12;">
   <a href>Forgot Your Password?</a> or <a href>Create a New Account</a>
 </div>
 HERE
