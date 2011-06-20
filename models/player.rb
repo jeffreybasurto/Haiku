2f5c77ec30e78f3edadb3ad399d1b766868343f1
Jeffrey Basurto <jeffreybasurto@gmail.com>
2011-06-20 12:16:43 -0500

    moved the player into a models directory

---
+++ models/player.rb
@@ -0,0 +1,29 @@
+# Player builds the interface between the world and the socket.
+class Player
+  attr_accessor :socket
+
+  def Player.list
+    @list ||= []
+  end
+
+  def Player.connected
+    Player.list
+  end
+  
+  def initialize
+    Player.list << self
+    
+    # verbs .. ugly for now, but we'll change later TODO
+    @verbs = {:say=>proc {|value| self.communicate(value) }} # Take the value from the packet "say" and pass it to communicate.
+  end
+    
+    
+  def send data
+    @socket.send(data) if @socket
+  end
+  
+  # in game communication of some kind.
+  def communicate value
+    Player.connected.each { |p| p.send(value) }  
+  end
+end
