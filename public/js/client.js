e3458a79239edc51037bb189e074d94f536d8e9b
Jeffrey Basurto <jeffreybasurto@gmail.com>
2011-06-21 19:05:19 -0500

    moved js out of view

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,49 @@
 var div_count = 0;
 
+$(function(){
+  ws = new WebSocket('ws://'+window.location.hostname+':8080');
+  ws.onmessage = function(e) { 
+      var received = JSON.parse(e.data);
+      if(received["form"]) {
+        var data = received["form"];
+        eval("var passed_buttons =" + data[1]["buttons"]); 
+        $("#"+scroll(data[0])).dialog({
+          show: data[1]["show"],
+          hide: data[1]["hide"],
+          title: data[1]["title"],
+          width: "auto",
+	        resizable: false,
+    		modal: true,
+    		buttons: passed_buttons
+    	  });
+      }
+      if(received["cmd"]) {
+        if(received["cmd"] == "clear_screen") {
+            $("#scrolling-region").empty();
+            console.log("Screen cleared.");
+        }
+      }
+      if(received["scrollback"]) {
+        $("button, input:submit, a", "#"+scroll(received["scrollback"])).button();
+      } 
+      if(received["dialog"]) {
+        $("#"+scroll(received["dialog"])).dialog({
+          resizable: false,
+			modal: true,
+			buttons: {
+				Ok: function() {
+					$( this ).dialog( "close" );
+				}
+			},
+			show: "highlight",
+			hide: "explode"
+		  });
+      }
+  };
+  ws.onclose = function() { debug("<span style=\"color:red;\">Socket closed.</span>"); };
+  ws.onopen = function() { debug("Connected."); };
+});
+
 function debug(str){ 
   $("#debug").empty();
   $("#debug").append(str+"<br"); 
