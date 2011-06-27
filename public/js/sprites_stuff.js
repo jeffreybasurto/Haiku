4ce3fd1aa6df149d6be36d024b886e8a320d53c1
Jeffrey Basurto <jeffreybasurto@gmail.com>
2011-06-27 04:34:41 -0500

    changed css and js

--- public/js/sprites_stuff.js
+++ public/js/sprites_stuff.js
@@ -1,13 +1,37 @@
 function init_sprites() {
   // Build the structure for the entire screen.
   var sprites_area = $("#sprite-area");
-  var y_total = 0;
-  var x_total = 0;
+  var y_total = 0,
+      x_total = 0,
+      y_reduced = false,
+      x_reduced = false;
   var y_to = Math.floor((sprites_area.height()) / 62);
   var x_to = Math.floor((sprites_area.width()) / 62);
+  if ((y_to) % 2 == 0) {
+    y_to = y_to - 1;
+    y_reduced = true;
+  }
+  if ((x_to) % 2 == 0) {
+    x_to = x_to - 1;
+    x_reduced = true;
+  }
   
-  sprites_area.css('margin-left', (sprites_area.width()) % 62 / 2);
-  sprites_area.css('margin-top', (sprites_area.height()) % 62 / 2);
+  
+  sprites_area.css('margin-left', sprites_area.width() % 62 / 2);
+  sprites_area.css('margin-right', sprites_area.width() % 62 / 2);
+  sprites_area.css('margin-top', sprites_area.height() % 62 / 2);
+  sprites_area.css('margin-bottom', sprites_area.height() % 62 / 2);
+  
+  if(x_reduced == true) {
+    sprites_area.css('margin-left', sprites_area.css('margin-left') + 31+"px");
+    sprites_area.css('margin-right', sprites_area.css('margin-right') + 31 +"px");
+  }
+  
+  if (y_reduced == true) {
+    sprites_area.css('margin-bottom', sprites_area.css('margin-bottom') + 31+"px");
+    sprites_area.css('margin-top', sprites_area.css('margin-top') + 31 +"px");
+  }
+  
   
   console.log(sprites_area.height());
   console.log(sprites_area.width());
